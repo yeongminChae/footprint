18aa01aa48a4c8e88b56fc2a50be5602e6087379
Yeongmin_Chae <codudals98@naver.com>
2024-04-09 12:07:37 +0900

    프로젝트에서 사용할 기본 생상 정리  + 그림자 효과 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      borderColor: "#929292",
-    },
     extend: {
+      colors: {
+        borderColor: "#929292",
+        bgColor: "#E9F7F3",
+        btnColor: "#008B8B",
+        unActiveBtnColor: "#B9B9B9",
+        hoverColor: "#4AB3B3",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        "custom-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
       },
     },
   },
   plugins: [],
 };
 export default config;
-
-// npm install -D tailwindcss postcss autoprefixer concurrently
-// npm install -D prettier prettier-plugin-tailwindcss

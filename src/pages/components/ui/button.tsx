3980e3e706ef0cdad52f42bc62d617768376f5da
Yeongmin_Chae <codudals98@naver.com>
2024-04-12 11:13:29 +0900

    버튼 컴포넌트, 액티브 여부에 따라 UI 수정되도록 구현 + 컴포넌트 호출시에 big, small에 따라 버튼 사이즈 조정되도록 구현

--- src/pages/components/ui/button.tsx
+++ src/pages/components/ui/button.tsx
@@ -1,21 +1,48 @@
+import cls from "@/libs/utils";
 import { useRouter } from "next/router";
 
 interface ButtonProps {
   btnTitle: string;
   moveUrl: string;
+  isActivate?: boolean;
+  size?: "big" | "small";
 }
 
-const Button = ({ btnTitle, moveUrl }: ButtonProps) => {
+const Button = ({
+  btnTitle,
+  moveUrl,
+  isActivate = true,
+  size = "big",
+}: ButtonProps) => {
   const router = useRouter();
+
+  const btnStyling = cls(
+    "transition-all ease-in duration-200 flex items-center justify-center shadow-custom-shadow rounded-lg ",
+    size === "big" ? "w-[28rem] h-16" : "w-36 h-12"
+  );
+
   const onBtnClick = () => {
     router.push(moveUrl);
   };
+
   return (
     <div
       onClick={onBtnClick}
-      className="w-[28rem] h-[4rem] bg-btnColor transition-all ease-in duration-200 flex items-center justify-center cursor-pointer shadow-custom-shadow rounded-lg hover:bg-hoverColor"
+      className={cls(
+        btnStyling,
+        isActivate
+          ? "bg-btnColor hover:bg-hoverColor cursor-pointer"
+          : "bg-unActiveBtnColor"
+      )}
     >
-      <span className="text-white font-bold text-xl">{btnTitle}</span>
+      <span
+        className={cls(
+          "text-white font-bold",
+          size === "big" ? " text-xl" : "text-base"
+        )}
+      >
+        {btnTitle}
+      </span>
     </div>
   );
 };

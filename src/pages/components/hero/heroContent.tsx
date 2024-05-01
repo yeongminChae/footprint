b48b3943f9a6a5feea0ce42fd5df44443eaefebe
Yeongmin_Chae <codudals98@naver.com>
2024-05-01 17:54:53 +0900

    구현 : sticky 에니메이션 기존 코드 수정 + framer-motion으로 등장 에니메이션 구현 framer-motion의 useScroll, useMotionValueEvent를 사용하여 sticky 에니메이션 구현, 기존 코드에서 하드코딩하여 값을 찾는 부분 최소화, 특히 이전에는 top에 값을 추정하기 위해 모든 경우의 수를 하드코딩 했었지만 지금은 framer-motion에 기능으로 해당 기능 간소화. framer-motion의 useInView를 사용해서 뷰포트에 해당 ui등장시 이벤트가 트리깅 되어 에니메이션이 작동 되도록 기능 구현

--- src/pages/components/hero/heroContent.tsx
+++ src/pages/components/hero/heroContent.tsx
@@ -1,67 +1,152 @@
-import { motion, MotionValue, useTransform } from "framer-motion";
+import { useRef, useState } from "react";
 
-import cls from "@/libs/utils";
+import {
+  useMotionValueEvent,
+  useScroll,
+  motion,
+  useInView,
+} from "framer-motion";
+
 import Button from "../ui/button";
+import cls from "@/libs/utils";
 
-export interface HeroContentProps {
+interface IHeroContentProps {
+  id: string;
   hero: JSX.Element;
   title?: string;
   description: string;
-  className?: string;
-  topValue?: string;
   spanOrButton: "span" | "button";
-  scrollYProgressProps?: MotionValue<number>;
 }
-export function HeroContent({
+
+const HeroContent = ({
+  id,
   hero,
   title,
   description,
-  className,
-  topValue,
   spanOrButton,
-  scrollYProgressProps,
-}: HeroContentProps) {
-  const opacity = scrollYProgressProps
-    ? useTransform(scrollYProgressProps, [0, 1], [1, 0])
-    : 1;
-  const scale = scrollYProgressProps
-    ? useTransform(scrollYProgressProps, [0, 1], [1, 1.3])
-    : 1;
+}: IHeroContentProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const svgRef = useRef<HTMLDivElement>(null);
+  const [isPopUp, setIsPopUp] = useState(false);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end start", "start start"],
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (progress) => {
+    if (progress <= 0.85 && !isPopUp) {
+      setIsPopUp(true);
+    } else if (progress > 0.85 && isPopUp) {
+      setIsPopUp(false);
+    }
+  });
+
+  const isInView = useInView(svgRef, { once: true });
 
   return (
-    <motion.div
-      className={cls(
-        "flex flex-col items-center justify-center gap-20 h-[34.75rem]",
-        className || ""
-      )}
-      style={{ top: topValue }}
-    >
-      <motion.div
-        className={cls(spanOrButton === "span" ? "" : "self-start")}
-        style={{ opacity }}
-      >
-        {hero}
-      </motion.div>
+    <section className="h-[150vh] w-full p-0 m-0">
       <div
-        className={cls(
-          "flex flex-col items-center justify-center gap-5",
-          spanOrButton === "span" ? "" : "mt-[-12.5rem]"
-        )}
+        id={id}
+        ref={ref}
+        className="flex items-center justify-center h-full"
       >
-        {spanOrButton === "span" ? (
-          <motion.span
-            style={{ scale }}
-            className="text-4xl font-bold tracking-wider"
+        <motion.div
+          className={cls(
+            "sticky flex items-center justify-center flex-col ",
+            id === "3" ? "top-10 gap-6" : "top-36 gap-10"
+          )}
+        >
+          <motion.div
+            ref={svgRef}
+            variants={divVariants}
+            initial="start"
+            animate={isInView ? "end" : ""}
+            transition={{
+              type: "tween",
+              delay: id == "1" ? 0.5 : 0.75,
+              duration: 0.5,
+            }}
           >
-            {title}
-          </motion.span>
-        ) : (
-          <Button btnTitle={"계산하기"} moveUrl={"/calculator"} />
-        )}
-        <motion.span style={{ scale }} className="text-xl font-bold opacity-60">
-          {description}
-        </motion.span>
+            {hero}
+          </motion.div>
+          {spanOrButton === "span" ? (
+            <TextSpan
+              text={title || ""}
+              isVisible={isPopUp}
+              delay={!isPopUp ? 0.5 : 0}
+              className={"text-4xl tracking-wider"}
+            />
+          ) : (
+            <ButtonDiv isVisible={isPopUp} />
+          )}
+          <TextSpan
+            text={description}
+            isVisible={isPopUp}
+            delay={isPopUp ? 0.5 : 0}
+            className={"text-xl opacity-60"}
+          />
+        </motion.div>
       </div>
+    </section>
+  );
+};
+
+interface TextSpanProps {
+  text: string;
+  isVisible: boolean;
+  delay: number;
+  className: string;
+}
+
+const TextSpan = ({ text, isVisible, delay, className }: TextSpanProps) => {
+  return (
+    <motion.span
+      variants={spanVariants}
+      initial="start"
+      animate={isVisible ? "end" : ""}
+      transition={{ delay, duration: 0.5 }}
+      className={cls("font-bold", className)}
+    >
+      {text}
+    </motion.span>
+  );
+};
+
+const ButtonDiv = ({ isVisible }: { isVisible: boolean }) => {
+  return (
+    <motion.div
+      variants={spanVariants}
+      initial="start"
+      animate={isVisible ? "end" : ""}
+      transition={{ delay: !isVisible ? 0.5 : 0, duration: 0.5 }}
+      className="-mt-24"
+    >
+      <Button btnTitle={"계산하기"} moveUrl={"/calculator"} />
     </motion.div>
   );
-}
+};
+
+const spanVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const divVariants = {
+  start: {
+    opacity: 0,
+    y: 30,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+export default HeroContent;

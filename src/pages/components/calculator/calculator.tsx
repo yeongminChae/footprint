982d95c2a963d7b01810f50348cf1e147119b04e
Yeongmin_Chae <codudals98@naver.com>
2024-05-03 23:53:16 +0900

    수정 : calculator 컴포넌트 중복되는 tailwindcss 요소들 변수화 + 긴 tailwindcss 요소들 변수화

--- src/pages/components/calculator/calculator.tsx
+++ src/pages/components/calculator/calculator.tsx
@@ -7,6 +7,7 @@ interface iCalculaotrFormProps {
   co2: string;
   units: string;
   keyword: string;
+  key: string;
 }
 
 const CalculatorForm = ({
@@ -18,44 +19,51 @@ const CalculatorForm = ({
 }: iCalculaotrFormProps) => {
   return (
     <>
-      <section className="w-[48rem] h-[28rem] text-btnColor  mt-7 bg-white rounded-lg border border-bordeColor shadow-xl">
-        <div className="h-20 px-7 flex flex-col justify-center border-b border-btnColor border-opacity-45">
+      <form className={formStyle}>
+        <div
+          className={cls(
+            borderOpacity40,
+            "h-20 px-7 flex flex-col justify-center border-b"
+          )}
+        >
           <span className="text-xl font-bold tracking-wide">{title}</span>
         </div>
 
-        <div className="px-10 pt-3 flex flex-col h-[calc(100%-5rem)] justify-center items-end gap-14">
-          <div className="flex w-full justify-between items-center gap-5">
+        <div className={inputStyle}>
+          <div className={cls(flexCenter, "w-full justify-between gap-5")}>
             <span className="text-lg font-medium">{inputTitle}</span>
             <input
               className={cls(
                 sharedStyles,
-                "border-btnColor border-opacity-80 text-sm text-end outline-none appearance-none placeholder:text-btnColor placeholder:opacity-45 placeholder:text-sm placeholder:right-4 placeholder:absolute"
+                placeHolderStyles,
+                "text-sm text-end outline-none appearance-none "
               )}
               placeholder="사용량을 입력해주세요..."
-              type="number"
+              type="string"
             />
             <span className="text-xs w-12 font-medium">{units} / 월</span>
           </div>
 
-          <div className="flex w-full justify-between items-center gap-5">
+          <div className={cls(flexCenter, "w-full justify-between gap-5")}>
             <span className="text-lg font-medium">CO₂ 발생량</span>
-            <div
-              className={cls(
-                sharedStyles,
-                "border-btnColor border-opacity-80 flex items-center justify-end"
-              )}
-            >
+            <div className={cls(sharedStyles, flexCenter, "justify-end")}>
               <span className="text-sm opacity-50 tracking-widest">{co2}</span>
             </div>
             <span className="text-xs w-12 font-medium">kg / 월</span>
           </div>
 
-          <div className="flex items-center justify-center border border-btnColor border-opacity-45 px-4 py-2 rounded-full">
+          <div
+            className={cls(
+              borderOpacity40,
+              flexCenter,
+              "justify-center border px-4 py-2 rounded-full"
+            )}
+          >
             <span className="text-xs">{keyword} CO₂ 발생량</span>
           </div>
         </div>
-      </section>
-      {/* <div className="w-[48rem] pt-12 flex items-center justify-between">
+      </form>
+      {/* <div {cls(className="w-[48rem] pt-12 flex items-center justify-between")}>
         <Button btnTitle={"이전"} moveUrl={"/"} size="small" />
         <Button btnTitle={"다음"} moveUrl={"/"} size="small" />
       </div> */}
@@ -65,42 +73,18 @@ const CalculatorForm = ({
 
 export default CalculatorForm;
 
-const sharedStyles = "border border-borderColor flex-grow h-11 rounded-lg pr-4";
+const formStyle =
+  "w-[48rem] h-[28rem] text-btnColor mt-7 bg-white rounded-lg border border-bordeColor shadow-xl";
 
-export const formConfigs = [
-  {
-    title: "전기 CO₂ 발생량",
-    inputTitle: "전기 사용량",
-    co2: (0.0).toFixed(1),
-    units: "kwh",
-    keyword: "전기",
-  },
-  {
-    title: "가스 CO₂ 발생량",
-    inputTitle: "가스 사용량",
-    co2: (0.0).toFixed(1),
-    units: "m³",
-    keyword: "가스",
-  },
-  {
-    title: "수도 CO₂ 발생량",
-    inputTitle: "수도 사용량",
-    co2: (0.0).toFixed(1),
-    units: "m³",
-    keyword: "수도",
-  },
-  {
-    title: "교통 CO₂ 발생량",
-    inputTitle: "교통 사용량",
-    co2: (0.0).toFixed(1),
-    units: "km",
-    keyword: "교통",
-  },
-  {
-    title: "생활 폐기물 CO₂ 발생량",
-    inputTitle: "폐기물 배출량",
-    co2: (0.0).toFixed(1),
-    units: "kg",
-    keyword: "폐기물",
-  },
-];
+const inputStyle =
+  "h-[calc(100%-5rem)] flex flex-col justify-center items-end gap-14 px-10 pt-3 ";
+
+const sharedStyles =
+  "flex-grow h-11 rounded-lg pr-4 border border-btnColor border-opacity-80";
+
+const placeHolderStyles =
+  "placeholder:text-btnColor placeholder:opacity-45 placeholder:text-sm placeholder:right-4 placeholder:absolute";
+
+const borderOpacity40 = "border-btnColor border-opacity-45";
+
+const flexCenter = "flex items-center";

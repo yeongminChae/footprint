4ce4ed79265000e8ab56bf201fcfe4791c7aff4f
Yeongmin_Chae <codudals98@naver.com>
2024-05-03 22:51:58 +0900

    구현 : calculator 컴포넌트와 formConfigs 객체를 사용하여 슬라이더 폼 구현 + calculator 슬라이더 컴포넌트를 이전 / 다음 버튼을 통해 이동 기능 구현 + calculator 슬라이더 에니메이션 기능 구현

--- src/pages/calculator/index.tsx
+++ src/pages/calculator/index.tsx
@@ -1,45 +1,121 @@
+import { useState } from "react";
+
+import { AnimatePresence, motion } from "framer-motion";
+
 import cls from "@/libs/utils";
-import Button from "../components/ui/button";
+import Arrow, { DisabledBtn } from "../components/calculator/arrow";
+import CalculatorForm, {
+  formConfigs,
+} from "../components/calculator/calculator";
 
 const Calculator = () => {
+  const [numAndIsPrev, setNumAndIsPrev] = useState({
+    number: 1,
+    isPrev: false,
+  });
+
+  const max = formConfigs.length;
+
+  const toNext = () => {
+    setNumAndIsPrev((prev) => {
+      const newNumber = prev.number === max ? max : prev.number + 1;
+      return { ...prev, number: newNumber, isPrev: false };
+    });
+  };
+  const toPrev = () => {
+    setNumAndIsPrev((prev) => {
+      const newNumber = prev.number === 1 ? 1 : prev.number - 1;
+      return { ...prev, number: newNumber, isPrev: true };
+    });
+  };
+
   return (
-    <div className="w-screen h-screen bg-bgColor bg-center flex items-center justify-center">
-      <section className="w-[48rem] h-[28rem] mt-7 bg-white rounded-2xl border border-borderColor">
-        <div className="h-20 px-7 flex flex-col justify-center border-b border-borderColor">
-          <span className="text-xl font-bold tracking-wide">
-            전기 CO₂ 발생량
-          </span>
-        </div>
+    <div className={container}>
+      {numAndIsPrev.number === 1 ? (
+        <DisabledBtn text="Prev" />
+      ) : (
+        <Arrow onClick={toPrev} left={true} />
+      )}
 
-        <div className="px-10 pt-3 flex flex-col h-[calc(100%-5rem)] justify-center items-end gap-14">
-          <div className="flex w-full justify-between items-center gap-5">
-            <span className="text-lg font-medium">전기 사용량</span>
-            <input
-              className={cls(
-                sharedStyles,
-                "text-end outline-none appearance-none text-sm placeholder:text-sm placeholder:right-4 placeholder:absolute"
-              )}
-              placeholder="사용량을 입력해주세요..."
-              type="number"
-            />
-            <span className="text-xs w-12 font-medium">kwh / 월</span>
-          </div>
+      <DivStacks />
 
-          <div className="flex w-full justify-between items-center gap-5">
-            <span className="text-lg font-medium">CO₂ 발생량</span>
-            <div className={cls(sharedStyles, "flex items-center justify-end")}>
-              <span className="text-sm opacity-50 tracking-widest">0.0</span>
-            </div>
-            <span className="text-xs w-12 font-medium">kg / 월</span>
-          </div>
+      <AnimatePresence custom={numAndIsPrev.isPrev}>
+        {formConfigs.map((config, idx) => {
+          return numAndIsPrev.number === idx + 1 ? (
+            <motion.div
+              key={config.keyword}
+              variants={slider}
+              custom={numAndIsPrev.isPrev}
+              initial="innumAndIsPrev"
+              animate="numAndIsPrev"
+              exit="exit"
+              className="absolute z-10 place-items-center justify-center -translate-y-[0.1rem]"
+            >
+              <CalculatorForm
+                title={config.title}
+                inputTitle={config.inputTitle}
+                co2={config.co2}
+                units={config.units}
+                keyword={config.keyword}
+                key={config.keyword}
+              />
+            </motion.div>
+          ) : null;
+        })}
+      </AnimatePresence>
 
-          <Button btnTitle={"다음"} moveUrl={"/"} size="small" />
-        </div>
-      </section>
+      {numAndIsPrev.number === max ? (
+        <DisabledBtn text="Next" />
+      ) : (
+        <Arrow onClick={toNext} left={false} />
+      )}
     </div>
   );
 };
 
 export default Calculator;
 
-const sharedStyles = "border border-borderColor flex-grow h-11 rounded-lg pr-4";
+const container =
+  "relative w-screen h-screen overflow-hidden bg-bgColor bg-center flex items-center justify-around gap-[55rem]";
+
+const DivStacks = () => {
+  const stackStyle =
+    "absolute rounded-lg border border-bordeColor shadow-xl h-[28rem] bg-white place-items-center";
+
+  return (
+    <>
+      <div className={cls("opacity-70 w-[42rem] translate-y-10", stackStyle)} />
+      <div className={cls("opacity-80 w-[44rem] translate-y-8", stackStyle)} />
+      <div
+        className={cls(
+          "opacity-90 w-[46rem] translate-y-[1.35rem]",
+          stackStyle
+        )}
+      />
+    </>
+  );
+};
+
+const slider = {
+  innumAndIsPrev: (isPrev: boolean) => ({
+    x: !isPrev ? 1500 : -1500,
+    opacity: 0,
+    scale: 0,
+  }),
+  numAndIsPrev: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: (isPrev: boolean) => ({
+    x: isPrev ? 1500 : -1500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.5,
+    },
+  }),
+};

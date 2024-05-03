303a4f60063477369042d02e0b3d52de7951539f
Yeongmin_Chae <codudals98@naver.com>
2024-05-04 00:05:15 +0900

    구현 : formConfigs, calculator 컴포넌트에서 분리

---
+++ src/pages/components/calculator/formConfigs.tsx
@@ -0,0 +1,37 @@
+export const formConfigs = [
+  {
+    title: "전기 CO₂ 발생량",
+    inputTitle: "전기 사용량",
+    co2: (0.0).toFixed(1),
+    units: "kwh",
+    keyword: "전기",
+  },
+  {
+    title: "가스 CO₂ 발생량",
+    inputTitle: "가스 사용량",
+    co2: (0.0).toFixed(1),
+    units: "m³",
+    keyword: "가스",
+  },
+  {
+    title: "수도 CO₂ 발생량",
+    inputTitle: "수도 사용량",
+    co2: (0.0).toFixed(1),
+    units: "m³",
+    keyword: "수도",
+  },
+  {
+    title: "교통 CO₂ 발생량",
+    inputTitle: "교통 사용량",
+    co2: (0.0).toFixed(1),
+    units: "km",
+    keyword: "교통",
+  },
+  {
+    title: "생활 폐기물 CO₂ 발생량",
+    inputTitle: "폐기물 배출량",
+    co2: (0.0).toFixed(1),
+    units: "kg",
+    keyword: "폐기물",
+  },
+];

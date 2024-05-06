0e152ed04d4d8bcc63556e6f1195cfacdf0da1d4
Yeongmin_Chae <codudals98@naver.com>
2024-05-06 11:52:22 +0900

    구현 : 각종 수정사항 적용 + calculator 폼에 유효성 여부에 따라 Next 버튼 disabled 여부 확인하는 코드 구현. 자식에서 부모컴포넌트로 데이터 보내는 방법 -> 1. 부모 컴포넌트에서 자식 컴포넌트로 콜백함수를 보낸다. 2. 자식 컴포넌트에서는 특정 이벤트가 발생하면 해당 콜백함수를 호출하여 데이터를 처리한 후 부모 컴포넌트로 보낸다.

--- src/pages/calculator/index.tsx
+++ src/pages/calculator/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { isValidElement, useEffect, useState } from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 
 import cls from "@/libs/utils";
-import Arrow, { DisabledBtn } from "../components/calculator/arrow";
+import Arrow, { DisabledArrow } from "../components/calculator/arrow";
 import CalculatorForm from "../components/calculator/calculator";
 import { formConfigs } from "../components/calculator/formConfigs";
 import { LeafIcon } from "../components/navBar";
@@ -13,6 +13,7 @@ const Calculator = () => {
     number: 1,
     isPrev: false,
   });
+  const [formValid, setFormValid] = useState(false);
 
   const max = formConfigs.length;
 
@@ -22,6 +23,7 @@ const Calculator = () => {
       return { ...prev, number: newNumber, isPrev: false };
     });
   };
+
   const toPrev = () => {
     setNumAndIsPrev((prev) => {
       const newNumber = prev.number === 1 ? 1 : prev.number - 1;
@@ -29,49 +31,55 @@ const Calculator = () => {
     });
   };
 
+  const isFormValid = (isValid: boolean) => {
+    setFormValid(isValid);
+  };
+
   return (
-    <div className={container}>
-      {numAndIsPrev.number === 1 ? (
-        <DisabledBtn text="Prev" />
-      ) : (
-        <Arrow onClick={toPrev} left={true} />
-      )}
-
-      <DivStacks />
-
-      <AnimatePresence custom={numAndIsPrev.isPrev}>
-        {formConfigs.map((config, idx) =>
-          numAndIsPrev.number === idx + 1 ? (
-            // 맵(map) 함수나 반복문 내에서 JSX 반환 식에서
-            // ()를 사용하면 return을 명시해 주지 않아도 되고,
-            // {}를 사용하면 return을 명시해 주어야 한다.
-            <motion.div
-              key={config.keyword}
-              variants={slider}
-              custom={numAndIsPrev.isPrev}
-              initial="innumAndIsPrev"
-              animate="numAndIsPrev"
-              exit="exit"
-              className="absolute z-10 place-items-center justify-center -translate-y-[0.1rem]"
-            >
-              <CalculatorForm
-                title={config.title}
-                inputTitle={config.inputTitle}
-                co2={config.co2}
-                units={config.units}
-                keyword={config.keyword}
-                key={config.keyword}
-              />
-            </motion.div>
-          ) : null
+    <div className="w-screen h-screen bg-bgColor bg-center flex flex-col">
+      <div className={container}>
+        {numAndIsPrev.number === 1 ? (
+          <DisabledArrow text="Prev" />
+        ) : (
+          <Arrow onClick={toPrev} left={true} />
         )}
-      </AnimatePresence>
 
-      {numAndIsPrev.number === max ? (
-        <DisabledBtn text="Next" />
-      ) : (
-        <Arrow onClick={toNext} left={false} />
-      )}
+        <DivStacks />
+
+        <AnimatePresence custom={numAndIsPrev.isPrev}>
+          {formConfigs.map((config, idx) =>
+            numAndIsPrev.number === idx + 1 ? (
+              <motion.div
+                key={config.keyword}
+                variants={slider}
+                custom={numAndIsPrev.isPrev}
+                initial="innumAndIsPrev"
+                animate="numAndIsPrev"
+                exit="exit"
+                className="absolute z-10 place-items-center justify-center -translate-y-[0.1rem]"
+              >
+                <CalculatorForm
+                  id={config.id}
+                  key={config.keyword}
+                  title={config.title}
+                  inputTitle={config.inputTitle}
+                  units={config.units}
+                  keyword={config.keyword}
+                  calcNum={config.calcNum}
+                  types={config.types}
+                  isFormValid={isFormValid}
+                />
+              </motion.div>
+            ) : null
+          )}
+        </AnimatePresence>
+
+        {numAndIsPrev.number === max || !formValid ? (
+          <DisabledArrow text="Next" />
+        ) : (
+          <Arrow onClick={toNext} left={false} />
+        )}
+      </div>
     </div>
   );
 };
@@ -79,7 +87,7 @@ const Calculator = () => {
 export default Calculator;
 
 const container =
-  "relative w-screen h-screen overflow-hidden bg-bgColor bg-center flex items-center justify-around gap-[55rem]";
+  "relative w-full h-full overflow-hidden flex items-center justify-around gap-[55rem]";
 
 const DivStacks = () => {
   const stackStyle =

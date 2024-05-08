31bbf0cdbeabc613a821c96f9f37154ed7712b94
Yeongmin_Chae <codudals98@naver.com>
2024-05-08 22:48:55 +0900

    구현 : results 페이지 기본적인 레이아웃 구현, 결과 안내 컴포넌트 디자인 작업

---
+++ src/pages/results/index.tsx
@@ -0,0 +1,40 @@
+import { CalculatorFormHeader } from "../components/calculator/calculator";
+
+const results = () => {
+  return (
+    <div className="h-screen w-screen pt-16 bg-bgColor flex items-center justify-center">
+      <div className="w-[65rem] h-[40rem] bg-white rounded-lg border-2 shadow-xl">
+        <CalculatorFormHeader title="결과 분석" />
+        <div className="grid grid-cols-2 grid-rows-2 w-full h-[calc(100%-5rem)]">
+          <div className=""></div>
+          <ResultsAnnounce />
+          <div className="col-span-2 "></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default results;
+
+const ResultsAnnounce = () => {
+  const textVariant = (text: string) => (
+    <span className="text-btnColor">{text}</span>
+  );
+
+  return (
+    <div className="flex flex-col gap-10 pt-5 font-semibold">
+      <span className="font-bold text-3xl text-btnColor">결과 안내</span>
+      <div>
+        {textVariant("email1@gmail.com")}
+        님의 이산화탄소(CO₂) 발생량 통계입니다.
+      </div>
+      <div>
+        {textVariant("email1@gmail.com")}
+        님은 이산화탄소 배출량은 총 {textVariant("37.3kg")} 이며, 평균 가정
+        {textVariant("307.1kg")} 보다 약 {textVariant("-90%")} 더 적게 배출하고
+        있습니다."
+      </div>
+    </div>
+  );
+};

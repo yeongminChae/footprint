883d7eb3100db5e44d721a3653c2ce89f54b34c4
Yeongmin_Chae <codudals98@naver.com>
2024-05-09 23:59:34 +0900

    구현 : 로컬 스토리지에 저장되어 있는 co2data를 가져와서, 차트들과, resultAnnounce에 사용할 수 있도록 데이터 가공화 작업

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,42 +1,33 @@
+import { useEffect, useState } from "react";
+
 import { CalculatorFormHeader } from "../components/calculator/calculator";
 import BarChart from "../components/results/barChart";
 import DonutChart from "../components/results/donutChart";
+import ResultsAnnounce from "../components/results/resultsAnnounce";
 
 const results = () => {
+  const [co2Data, setCo2Data] = useState({});
+  const newCo2Data = Object.values(co2Data).map((i) => Number(i));
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("co2Data");
+    setCo2Data(storedData ? JSON.parse(storedData) : {});
+  }, []);
+
   return (
     <div className="h-screen w-screen pt-16 bg-bgColor flex items-center justify-center">
-      <div className="w-[65rem] h-[40rem] bg-white rounded-lg border-2 shadow-xl">
+      <section className="w-[65rem] h-[40rem] bg-white rounded-lg border-2 shadow-xl">
         <CalculatorFormHeader title="결과 분석" />
-        <div className="grid grid-cols-2 grid-rows-2 w-full h-[calc(100%-5rem)]">
-          <DonutChart />
-          <ResultsAnnounce />
-          <BarChart />
+        <div className={gridContainer}>
+          <DonutChart myDataArr={newCo2Data} />
+          <ResultsAnnounce myDataArr={newCo2Data} />
+          <BarChart myDataArr={newCo2Data} />
         </div>
-      </div>
+      </section>
     </div>
   );
 };
+
+const gridContainer = "grid grid-cols-2 grid-rows-2 w-full h-[calc(100%-5rem)]";
 
 export default results;
-
-const ResultsAnnounce = () => {
-  const textVariant = (text: string) => (
-    <span className="text-btnColor">{text}</span>
-  );
-
-  return (
-    <div className="col-start-2 row-start-1 flex flex-col gap-6 pt-5 font-semibold">
-      <span className="font-bold text-3xl text-btnColor">결과 안내</span>
-      <div>
-        {textVariant("email1@gmail.com")}
-        님의 이산화탄소(CO₂) 발생량 통계입니다.
-      </div>
-      <div>
-        {textVariant("email1@gmail.com")}
-        님은 이산화탄소 배출량은 총 {textVariant("37.3kg")} 이며, 평균 가정
-        {textVariant("307.1kg")} 보다 약 {textVariant("-90%")} 더 적게 배출하고
-        있습니다."
-      </div>
-    </div>
-  );
-};

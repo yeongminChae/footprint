d208977e7a06f043884c83e55f30ab06dc1b0ed2
Yeongmin_Chae <codudals98@naver.com>
2024-05-11 16:14:32 +0900

    구현 : bar차트 데이터레이블 호버시 저장한 데이터를 바탕으로 비교한 값을 UI에 그리는 기능 구현, 배출량 만큼 적은지 많은지에 따라 다르게 UI에 그리도록 기능 구현 + 리팩토링

--- src/pages/components/results/resultsAnnounce.tsx
+++ src/pages/components/results/resultsAnnounce.tsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 
+import { useRecoilValue } from "recoil";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { AnimatePresence, motion } from "framer-motion";
+
+import cls from "@/libs/utils";
+import { axisCartegory } from "@/libs/atoms";
 
 const ResultsAnnounce = ({ myDataArr }: { myDataArr: number[] }) => {
   const [date, setDate] = useState(Date);
-
-  const totalAveDataArr = 201.5;
-  const totalMyDataArr = Math.round(
-    myDataArr.reduce((acc, cur) => acc + cur, 0)
-  );
-  const ratio = Math.round((totalMyDataArr / totalAveDataArr) * 100);
-  const testedDate = format(new Date(date), "yyyy년 MM월 dd일", { locale: ko });
+  const testedDate = format(new Date(date), "yyyy년 MM월 dd일 a", {
+    locale: ko,
+  });
 
   useEffect(() => {
     const storedDate = localStorage.getItem("date");
@@ -19,7 +20,7 @@ const ResultsAnnounce = ({ myDataArr }: { myDataArr: number[] }) => {
   }, []);
 
   return (
-    <div className="col-start-2 row-start-1 flex flex-col gap-4 pt-5 px-4 font-semibold">
+    <div className={container}>
       <span className="font-bold text-3xl text-btnColor">결과 안내</span>
       <span>검사 일시 : {textVariant(testedDate)}</span>
       <div>
@@ -27,17 +28,73 @@ const ResultsAnnounce = ({ myDataArr }: { myDataArr: number[] }) => {
         님의 이산화탄소(CO₂) 발생량 통계입니다.
       </div>
       <div className="break-keep">
-        {textVariant("email1@gmail.com")}
-        님은 이산화탄소 배출량은 총 {textVariant(totalMyDataArr + "kg")} 이며,
-        평균 가정 배출량 {textVariant(`${totalAveDataArr} kg `)} 보다 약{" "}
-        {textVariant(ratio + "%")} 더 적게 배출하고 있습니다."
+        <AveTextComponent myDataArr={myDataArr} />
       </div>
+      <MyDataTextComponent />
     </div>
   );
 };
 
+const container =
+  "col-start-2 row-start-1 flex flex-col gap-4 pt-5 px-4 font-semibold";
+
 const textVariant = (text: string) => (
   <span className="text-btnColor">{text}</span>
 );
+
+const lessMoreSameVariant = (ratio: number) => {
+  const text = ratio > 100 ? "많이" : "적게";
+  const textStyle = text === "많이" ? "text-red-400" : "text-btnColor";
+
+  return (
+    <span className={cls(textStyle, "font-semibold")}>
+      {text === "적게"
+        ? `${(100 - ratio).toLocaleString()}% `
+        : `${ratio.toLocaleString()}% `}
+      {text}
+    </span>
+  );
+};
+
+const AveTextComponent = ({ myDataArr }: { myDataArr: number[] }) => {
+  const totalAveDataArr = 201.5;
+  const totalMyDataArr = Math.round(
+    myDataArr.reduce((acc, cur) => acc + cur, 0)
+  );
+  const ratio = Math.round((totalMyDataArr / totalAveDataArr) * 100);
+
+  return (
+    <div className="break-keep">
+      {textVariant("email1@gmail.com")}
+      님은 이산화탄소 배출량은 총{" "}
+      {textVariant(totalMyDataArr.toLocaleString() + "kg")} 이며, 평균 가정
+      배출량 {textVariant(`${totalAveDataArr} kg `)} 보다 약{" "}
+      {lessMoreSameVariant(ratio)} 배출하고 있습니다."
+    </div>
+  );
+};
+
+const MyDataTextComponent = () => {
+  const category = useRecoilValue(axisCartegory);
+  const { key, myValue, aveValue } = category;
+  const ratio = Math.round((category.myValue / category.aveValue) * 100);
+
+  return (
+    <AnimatePresence>
+      {category.key == "" && category.myValue == 0 ? null : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.2, duration: 0.5 } }}
+          exit={{ opacity: 0 }}
+          className="break-keep"
+        >
+          {textVariant(key)} 배출량은 총 {textVariant(myValue.toString())}kg
+          이고 이는 평균 배출량 {textVariant(aveValue.toString())}kg 보다 약{" "}
+          {lessMoreSameVariant(ratio)} 배출하고 있습니다.
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
 
 export default ResultsAnnounce;

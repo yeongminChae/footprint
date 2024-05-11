6ea48f139e3054f9f591225d0bc81bfc652c4005
Yeongmin_Chae <codudals98@naver.com>
2024-05-11 16:11:43 +0900

    구현 : bar차트에서, bar에 데이터 레이블을 호버하면, 해당 데이터와 평균값을 비교할 수 있도록, 호버시에 해당 데이터레이블에 데이터를 atom에 저장하는 기능 구현

--- src/libs/atoms.ts
+++ src/libs/atoms.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, RecoilEnv } from "recoil";
 
 export const co2Atom = atom({
   key: "co2",
@@ -9,3 +9,10 @@ export const co2DataAtom = atom({
   key: "co2Data",
   default: {},
 });
+
+export const axisCartegory = atom({
+  key: "cartegory",
+  default: { key: "", myValue: 0, aveValue: 0 },
+});
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
--- src/pages/components/navBar.tsx
+++ src/pages/components/navBar.tsx
@@ -83,8 +83,18 @@ export const LeafIcon = (
       strokeLinejoin="round"
       className="cursor-pointer fill-none stroke-[#98D7C2]"
     >
-      <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z" />
-      <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12" />
+      <motion.path
+        initial={{ pathLength: 0, pathOffset: 1 }}
+        animate={{ pathLength: 1, pathOffset: 0 }}
+        transition={{ duration: 2 }}
+        d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"
+      />
+      <motion.path
+        initial={{ pathLength: 0, pathOffset: 1 }}
+        animate={{ pathLength: 1, pathOffset: 0 }}
+        transition={{ duration: 2 }}
+        d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"
+      />
     </svg>
   );
 };
